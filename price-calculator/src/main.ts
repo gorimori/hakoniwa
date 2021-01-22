7aaf75ddd096423230c4311ac5d6a140c3096929
shuii <34891695+shu7ii@users.noreply.github.com>
2021-01-23 02:21:43 +0900

    price-calculator - variations

--- price-calculator/src/main.ts
+++ price-calculator/src/main.ts
@@ -6,7 +6,7 @@ type Plan = {
   readonly price: Price;
 };
 
-const plans: readonly Plan[] = [
+const samplePlans: readonly Plan[] = [
   {
     // 10
     users: 10,
@@ -30,54 +30,91 @@ const plans: readonly Plan[] = [
     users: 50,
     price: 465,
   },
-].sort((a, b) => {
-  // TODO: 必要？
-  // usersの降順に並べ替える
-  return b.users - a.users;
-});
+];
 
-type Result = {
-  readonly price: Price;
-  readonly composition: readonly Plan[];
-};
+const total = (plans: readonly Plan[]): Price =>
+  plans.reduce((sum, current) => sum + current.price, 0);
 
-/**
- * 結果を格納する
- *
- * 10ユーザー -> 100円
- *
- * TODO: buildCalculator(configs)(users) みたいな高階関数にしたい
- *
- */
-const res: Map<Users, Result> = new Map(
-  // 計算しなくてもわかるものは最初から入れておく
-  plans.map(({ price, users }) => [
-    users,
-    { price, composition: [{ users, price }] },
-  ])
-);
+class Result {
+  #current: Plan[] = [{ price: Infinity, users: NaN }];
 
-const calc = (users: Users): Result => {
-  const cached = res.get(users);
+  update(candidate: readonly Plan[]) {
+    if (total(this.#current) <= total(candidate)) {
+      return;
+    }
 
-  if (cached) {
-    return cached;
+    this.#current = [...candidate];
   }
 
-  const updateCache = (result: Result) => {
-    const current = res.get(users);
-    console.log(result);
+  get(): Plan[] {
+    return this.#current;
+  }
+}
 
-    if (current == undefined) {
-      res.set(users, result);
+class Simulator {
+  #cache: Map<Users, Result> = new Map();
+  #plans: readonly Plan[];
+
+  constructor(plans: readonly Plan[]) {
+    this.#plans = [...plans];
+  }
+
+  private getCachedPlans(users: Users): Plan[] | undefined {
+    return this.#cache.get(users)?.get();
+  }
+
+  private updateCache(users: Users, candidate: readonly Plan[]): void {
+    if (this.#cache.has(users)) {
+      this.#cache.get(users)?.update(candidate);
       return;
     }
 
-    if (result.price < current.price) {
-      res.set(users, result);
-      return;
+    const result = new Result();
+    result.update(candidate);
+    this.#cache.set(users, result);
+  }
+
+  calc(users: Users): Plan[] {
+    if (this.#cache.has(users)) {
+      return this.getCachedPlans(users) ?? [];
     }
-  };
+
+    const aux = (
+      currentIndex: number,
+      accPrice: Price,
+      restUsers: Users,
+      selectedPlans: readonly Plan[]
+    ): void => {
+      console.log("class, aux");
+      if (restUsers <= 0) {
+        this.updateCache(users, selectedPlans);
+        return;
+      }
+
+      const plan = this.#plans[currentIndex];
+      if (plan == undefined) {
+        return;
+      }
+
+      // 現在見ている価格設定を採用する場合
+      // 複数選択可能にしたいのでcurrentIndexは増やさない
+      aux(currentIndex, accPrice + plan.price, restUsers - plan.users, [
+        ...selectedPlans,
+        plan,
+      ]);
+
+      // 現在見ている価格設定を採用しない場合
+      aux(currentIndex + 1, accPrice, restUsers, selectedPlans);
+    };
+
+    aux(0, 0, users, []);
+
+    return this.getCachedPlans(users) ?? [];
+  }
+}
+
+export const buildCalc = (plans: readonly Plan[]) => (users: Users): Plan[] => {
+  const result = new Result();
 
   const aux = (
     currentIndex: number,
@@ -85,9 +122,9 @@ const calc = (users: Users): Result => {
     restUsers: Users,
     selectedPlans: readonly Plan[]
   ): void => {
+    console.log("closure, aux");
     if (restUsers <= 0) {
-      updateCache({ composition: selectedPlans, price: accPrice });
-      // console.log(JSON.stringify(selectedConfigs));
+      result.update(selectedPlans);
       return;
     }
 
@@ -109,7 +146,13 @@ const calc = (users: Users): Result => {
 
   aux(0, 0, users, []);
 
-  return res.get(users) ?? { price: Infinity, composition: [] };
+  return result.get();
 };
 
+const calc = buildCalc(samplePlans);
 console.log(calc(40));
+console.log(calc(80));
+
+// const sim = new Simulator(samplePlans);
+// console.log(sim.calc(40));
+// console.log(sim.calc(80));
